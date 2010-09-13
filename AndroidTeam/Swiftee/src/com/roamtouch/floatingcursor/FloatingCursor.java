72f3ea3b99e58e3cce41d382b388315f57d3174e
fabian <fabian@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2010-09-13 02:41:24 +0000

    Reset cursor on touch point reset.

--- AndroidTeam/Swiftee/src/com/roamtouch/floatingcursor/FloatingCursor.java
+++ AndroidTeam/Swiftee/src/com/roamtouch/floatingcursor/FloatingCursor.java
@@ -729,6 +729,7 @@ public class FloatingCursor extends FrameLayout implements MultiTouchObjectCanva
 				pointer.scrollTo(0,0);
 				fcPointerView.scrollTo(0,0);
 				fcProgressBar.scrollTo(0,0);
+				pointer.setImageResource(R.drawable.no_target_cursor);
 			}
 
 			mTouchPointValid = false;
