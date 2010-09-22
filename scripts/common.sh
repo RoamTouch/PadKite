a40b0635f7147e7a2e8580a685935155f0a6b9a3
fabian <fabian@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2010-09-22 00:08:57 +0000

    Make it possible to use a system specific suffix (-eclair, -froyo).

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 REL="Beta"
+SUFFIX="-eclair"
 
 # Find actual md5sum
 
 [ ! -d .git ] && { echo "Please run only from toplevel directory." 1>&2; exit 1; }
 
 LIBSTR=$(md5 AndroidTeam/Swiftee/libs/RoamTouchWebKit.jar AndroidTeam/Swiftee/libs/armeabi/librtwebcore.so | cut -d'=' -f2 | awk '{ printf("%s\\/", substr($0, 2, 6)); }' | awk '{ print substr($0,0,14); }')
-VER=$(cat AndroidTeam/Swiftee/AndroidManifest.xml | grep android:versionName | perl -pi -e 's/.*="(.*)"/\1/')
+VER=$(cat AndroidTeam/Swiftee/AndroidManifest.xml | grep android:versionName | perl -pi -e 's/.*="(.*)"/\1/')$SUFFIX
