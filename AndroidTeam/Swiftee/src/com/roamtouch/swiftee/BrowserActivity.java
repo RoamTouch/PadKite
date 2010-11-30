7cbfef7a2c88f156c3bc7dca2ae499dff7cad6d7
fabian <fabian@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2010-11-30 02:20:58 +0000

    Release 1.43.6 with newest changes.

--- AndroidTeam/Swiftee/src/com/roamtouch/swiftee/BrowserActivity.java
+++ AndroidTeam/Swiftee/src/com/roamtouch/swiftee/BrowserActivity.java
@@ -67,8 +67,8 @@ public class BrowserActivity extends Activity implements OnGesturePerformedListe
 	
 	public static int DEVICE_WIDTH,DEVICE_HEIGHT;
 
-	public static String version = "Version Beta-v1.43.5-eclair build #299fbf/3e5dda";
-	public static String version_code = "Version Beta-v1.43.5";
+	public static String version = "Version Beta-v1.43.6-eclair build #862e37/105d79";
+	public static String version_code = "Version Beta-v1.43.6";
 	
 	final public static boolean developerMode = false;
 	
