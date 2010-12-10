967d0c4093c16a8e7cf2032bc00690da625c851c
fabian <fabian@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2010-12-10 18:43:29 +0000

    Add chinese language.

--- AndroidTeam/Swiftee/src/com/roamtouch/swiftee/Translater.java
+++ AndroidTeam/Swiftee/src/com/roamtouch/swiftee/Translater.java
@@ -10,18 +10,26 @@ public class Translater {
 		
 	    try {
 	    	GoogleAPI.setHttpReferrer("http://code.google.com/p/google-api-translate-java/");
-	    	Language to = Language.ENGLISH;
 
-	    	if(toLanguage.equals("RUSSIAN"))
-	    		to = Language.RUSSIAN;
-	    	else if(toLanguage.equals("FRENCH"))
-	    		to = Language.FRENCH;
-	    	else if(toLanguage.equals("GERMAN"))
-	    		to = Language.GERMAN;
-	    	else if(toLanguage.equals("SPANISH"))
-	    		to = Language.SPANISH;
-	    	else if(toLanguage.equals("HINDI"))
-	    		to = Language.HINDI;
+	    	Language to = Language.fromString(toLanguage);
+	    	
+	    	if (to == null)
+	    	{
+	    		to = Language.ENGLISH;
+
+	    		if(toLanguage.equals("RUSSIAN"))
+	    			to = Language.RUSSIAN;
+	    		else if(toLanguage.equals("FRENCH"))
+	    			to = Language.FRENCH;
+	    		else if(toLanguage.equals("GERMAN"))
+	    			to = Language.GERMAN;
+	    		else if(toLanguage.equals("SPANISH"))
+	    			to = Language.SPANISH;
+	    		else if(toLanguage.equals("HINDI"))
+	    			to = Language.HINDI;
+	    		else if(toLanguage.equals("CHINESE"))
+	    			to = Language.CHINESE;
+	    	}
 	 
 	    	
 			String translatedText = Translate.execute(text, Language.ENGLISH, to);
