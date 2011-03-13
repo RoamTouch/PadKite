503c5cbd295fc36ea8a126a1fa8738902871274c
ec2 <ec2@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2011-03-13 20:09:14 +0000

    Fix all builds.

--- scripts/osb-all.sh
+++ scripts/osb-all.sh
@@ -2,7 +2,7 @@
 
 cd /android/roamtouch_android/ || exit 1
 
-for branch in $(git branch -l)
+for branch in $(git branch -l | sed 's/\*//g')
 do
 	echo "============== Building $branch ..."
 	$(dirname $0)/osb.sh "$branch" "$1"
