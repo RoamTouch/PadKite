6521d9e8d1c1dbb750e09efbec098577f0e21499
ec2 <ec2@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2011-03-13 17:06:05 +0000

    Add release script for ec2

---
+++ scripts/release-version-ec2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /android/roamtouch_android
+
+source $(dirname $0)/common.sh || exit 1
+
+echo -n "Releasing Version \"$REL-v$VER #$LIBSTR\" ... "
+
+cp AndroidTeam/Swiftee/bin/PadKite-release.apk releases/"PadKite-$REL-v$VER.apk"
+
+echo "done"
+
+# Sent mail notification here
