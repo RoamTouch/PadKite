e5945cd812d1b460ec48f711149a5a410c410b27
jose <jose@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2011-04-30 21:06:15 +0000

    Imported Multi wrong, erasing to import again.

--- AndroidTeam/src/com/api/twitter/Keys.java
+++
@@ -1,8 +0,0 @@
-package com.api.twitter;
-
-// You either come up with your own MyKeys class or you take out the extension and un-comment the values here
-// with your own token and key. Let's see how this works as far as me not giving away my stuff all the time!
-public class Keys {
-	public static final String TWITTER_CONSUMER_KEY = "1YGRCBARMUzbZlsJYuxnQ";
-	public static final String TWITTER_CONSUMER_SECRET = "0I1AwFGbnR0vtUORhqVV0PUhgOpc1Z2IuX07YRZ79DE";
-}
