d8cfe49617c9a7672502c9b10090a5577354e72a
jose <jose@ca52e890-a327-480e-99e1-443e3fe6cbb9>
2011-05-09 17:20:43 +0000

    Adding Twitter trends classes

---
+++ AndroidTeam/PadKite_Single/src/com/api/twitter/TwitterTrend.java
@@ -0,0 +1,34 @@
+package com.api.twitter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwitterTrend {
+	
+	Map<Object,String> mp = new HashMap<Object, String>();
+	
+    private String name;
+    private String url;
+    private String query;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getQuery() {
+        return query;
+    }
+    public void setQuery(String name) {
+        this.query = query;
+    }    
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+}
---
+++ AndroidTeam/PadKite_Single/src/com/api/twitter/TwitterTrends.java
@@ -0,0 +1,76 @@
+package com.api.twitter;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URI;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.gson.Gson;
+import android.util.Log;
+import com.api.twitter.TwitterTrend;
+
+public class TwitterTrends {
+	
+    private String as_of;
+    private List<TwitterTrend> trends;
+    
+    public String getAs_of() {
+        return as_of;
+    }
+    public void setAs_of(String asOf) {
+        as_of = asOf;
+    }
+    public List<TwitterTrend> getTrends() {
+        return trends;
+    }
+    public void setTrends(List<TwitterTrend> trends) {
+        this.trends = trends;
+    } 
+    
+    public void runJSONParser(){
+        try{
+	        Log.i("MY INFO", "Json Parser started..");
+	        Gson gson = new Gson();
+	        Reader r = new InputStreamReader(getJSONData("http://api.twitter.com/1/trends.json"));
+	        
+	        /**
+	         * TODO http://api.twitter.com/1/trends/"+location+".json (44418)
+	         * While using this trend with location parameter the result is an object 
+	         * MyObject[] jsonObjects = gson.fromJson(jsonText, MyObject[].class); or
+	         * Map<String, Object>[] result = gson.fromJson(jsonText, HashMap[].class);
+	         **/
+	        
+	        Log.i("MY INFO", ""+r.toString());
+	        TwitterTrends objs = gson.fromJson(r, TwitterTrends.class);
+	        Log.i("MY INFO", "CACA: "+objs.getTrends().size());
+	        for(TwitterTrend tr : objs.getTrends()){
+	            Log.i("TRENDS", tr.getName() + " - " + tr.getUrl());
+	        }
+	        }catch(Exception ex){
+	        	Log.i("MY INFO", "ex: "+ex);
+	            ex.printStackTrace();
+        }
+    }
+    
+    public InputStream getJSONData(String url){
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        URI uri;
+        InputStream data = null;
+        try {
+            uri = new URI(url);
+            HttpGet method = new HttpGet(uri);
+            HttpResponse response = httpClient.execute(method);
+            data = response.getEntity().getContent();
+        } catch (Exception e) {
+        	Log.i("MY INFO", "exeeee: "+e);
+            e.printStackTrace();
+        }        
+        return data;
+    }
+    
+}
